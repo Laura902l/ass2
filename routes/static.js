4543adc4e55491e9a6c7c16276c5182f8a753e99
Laura902l <121919112+Laura902l@users.noreply.github.com>
2024-01-28 03:15:51 +0600

    Update static.js

--- routes/static.js
+++ routes/static.js
@@ -5,11 +5,9 @@ const app = express();
 app.set('view engine', 'ejs');
 
 const router = express.Router();
-var pic1 = "/img/img_1.jpg";
-var pic2 = "/img/img_2.jpg";
 
 router.get('/', (req, res) => {
-    res.render('home', { pic1, pic2 }); // Pass pic1 and pic2 as properties to the template
+    res.render('home',{}); 
 });
 
 router.get('/contacts', (req, res) => {
