797dc429d9b98a757cba312024ec54a164086e8a
Laura902l <121919112+Laura902l@users.noreply.github.com>
2024-01-28 02:51:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//server.js
 const express = require('express');
-const bodyParser = require('body-parser'); // Import body-parser
+const bodyParser = require('body-parser');
+const path = require('path'); 
 
 const app = express();
 const port = 3000;
@@ -10,43 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const travelRoutes = require('./routes/travelRoutes');
-const staticRoutes = require('./routes/static'); // Corrected the require statement
-const tourHistoryRoutes = require('./routes/tourHistoryRoutes'); // Add this line
-app.use('/tourHistory', tourHistoryRoutes); // Add this line
-
-
+const staticRoutes = require('./routes/static'); 
+const history = require('./routes/tourHistoryRoutes');
 app.use('/travel', travelRoutes);
 app.use('/', staticRoutes);
-
-app.use(bodyParser.json());
-
-// Считать данные из файла tours.json
-let tourHistory = require('./tours.json');
-
-app.post('/travelagency', (req, res) => {
-    const { city, country, description } = req.body;
-
-    // Assuming you have some logic to add the tour to the database or tourHistory array
-    // For simplicity, let's just push the new tour into the tourHistory array
-    tourHistory.push({
-        city,
-        country,
-        description,
-        // Add other properties as needed
-    });
-
-    const newTour = {
-        city,
-        country,
-        description,
-        // Add other properties as needed
-    };
-
-    res.json({ message: 'Tour added successfully', newTour });
-});
-
-
-// Запускаем сервер
+app.use('/', history);
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
